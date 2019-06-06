5339829f4209f47ff315f4138606571184c7cd08
Johnny Bravo <SanValakas@mail.ru>
2019-06-06 14:56:22 +0300

    scroll fix

---
+++ js/one-page-scroll.js
@@ -0,0 +1,6 @@
+const wrapper = document.querySelector('.wrapper'),
+      onePageScroll = document.querySelector('.one-page-scroll'),
+      wrapperSectionsCollection = document.querySelectorAll('.section'),
+      wrapperSectionsCollectionLength = wrapperSectionsCollection.length;
+
+console.log(wrapperSectionsCollectionLength)
