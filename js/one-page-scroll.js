90a83dd09ed6982385dda7d2a74a7f7744e21007
Johnny Bravo <SanValakas@mail.ru>
2019-06-07 21:28:20 +0300

    mobile scroll

--- js/one-page-scroll.js
+++ js/one-page-scroll.js
@@ -69,3 +69,12 @@ OnePageScroll({
 	sideNavigation: 'fixed-nav__item',
 	attribute: 'data-scroll'
 });
+
+let checkMobile = () => {
+	let isMobile = false;
+	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Opera Mobile|Kindle|Windows Phone|PSP|AvantGo|Atomic Web Browser|Blazer|Chrome Mobile|Dolphin|Dolfin|Doris|GO Browser|Jasmine|MicroB|Mobile Firefox|Mobile Safari|Mobile Silk|Motorola Internet Browser|NetFront|NineSky|Nokia Web Browser|Obigo|Openwave Mobile Browser|Palm Pre web browser|Polaris|PS Vita browser|Puffin|QQbrowser|SEMC Browser|Skyfire|Tear|TeaShark|UC Browser|uZard Web|wOSBrowser|Yandex.Browser mobile/i.test(navigator.userAgent)) isMobile = true;
+	return isMobile;
+};    
+if (checkMobile) {
+			swipeDetected(content);
+	}
