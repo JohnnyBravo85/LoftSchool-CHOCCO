57987ad2b2706d642b5368934384f909bad2ed63
Johnny Bravo <SanValakas@mail.ru>
2019-06-13 18:13:35 +0300

    video

--- src/js/video-player.js
+++ src/js/video-player.js
@@ -1,103 +1,50 @@
-;(function(){
+;
+(function () {
   window.onload = function () {
-    let video = document.querySelector('#video');
-    video.addEventListener('click', playStop);
 
-    let playButtons = document.querySelectorAll('.play');
+    const play = document.querySelectorAll('.play'),
+      playLength = play.length,
+      videoPlayer = document.querySelector('.video__player')
+    video = document.querySelector('#video'),
+      videoPlayBigButton = document.querySelector('.video__play'),
+      videoToolbarPlayButton = document.querySelector('.video__toolbar-play'),
+      videoToolbarPauseButton = document.querySelector('.video__toolbar-pause');
 
-    //Навесили оброботчик событий на плей кнопки
-    for (let i = 0; i < playButtons.length; i++) {
-      playButtons[i].addEventListener('click', playStop);
-    };
-
-    let soundButton = document.querySelector('.video__volume-icon');
-
-    //Навесили выключение звука на иконку
-    soundButton.addEventListener('click', soundOff);
-
-    //Навесили события на ползунок времени
-    let durationControl = document.querySelector('#duration-time');
-    durationControl.addEventListener('mousedown', stopInterval);
-    durationControl.addEventListener('click', setVideoDuration)
-
-    //Присволили min значение ползунку
-    durationControl.min = 0;
-    durationControl.value = 0;
-
-    let soundControl = document.querySelector('#volume-level');
-    soundControl.addEventListener('mouseup', changeSoundVolume);
-
-    //Значения громкости
-    soundControl.min = 0;
-    soundControl.max = 10;
-
-    let videoPlayButton = document.querySelector('.video__play');
-    let toolbarPlayButton = document.querySelector('.video__toolbar-play');
-
-    //Функция событий после окончания видео
-    video.addEventListener('ended', function () {
-      videoPlayButton.classList.toggle('video__play--active');
-      toolbarPlayButton.classList.toggle('video__toolbar-play--active');
-      video.currentTime = 0;
+    videoPlayer.addEventListener('click', function(e) {
+      if(video.paused) {
+        video.play();
+        videoPlayBigButton.classList.add('video__play--active');
+        videoToolbarPlayButton.classList.add('video__toolbar-play--active');
+        videoToolbarPauseButton.classList.add('video__toolbar-pause--active');
+      } else {
+        video.pause();
+        videoPlayBigButton.classList.remove('video__play--active');
+        videoToolbarPlayButton.classList.remove('video__toolbar-play--active');
+        videoToolbarPauseButton.classList.remove('video__toolbar-pause--active');
+      }
     })
 
-    //Функция синхронизации ползунка и времени видео
-    let intervalId;
+    for (let i = 0; i < playLength; ++i) {
+      play[i].addEventListener('click', function (e) {
+        if (video.paused) {
+          video.play();
+          videoPlayBigButton.classList.add('video__play--active');
+          videoToolbarPlayButton.classList.add('video__toolbar-play--active');
+          videoToolbarPauseButton.classList.add('video__toolbar-pause--active');
+        } else {
+          video.pause();
+          videoPlayBigButton.classList.remove('video__play--active');
+          videoToolbarPlayButton.classList.remove('video__toolbar-play--active');
+          videoToolbarPauseButton.classList.remove('video__toolbar-pause--active');
+        }
+      });
+    }
 
-    function playStop() {
-      videoPlayButton.classList.toggle('video__play--active');
-      toolbarPlayButton.classList.toggle('video__toolbar-play--active');
+  
 
-      durationControl.max = video.duration;
 
-      if (video.paused) {
-        video.play();
-        intervalId = setInterval(updateDuration, 1000 / 66)
 
-      } else {
-        video.pause();
-        clearInterval(intervalId);
-      };
-    };
 
-    //Функция очистки интервала
-    function stopInterval() {
-      video.pause();
-      clearInterval(intervalId);
-    };
 
-    //Функция управления видео во время пауз
-    function setVideoDuration() {
-      if (video.paused) {
-        video.play();
-      } else {
-        video.pause();
-      }
-      video.currentTime = durationControl.value;
-      intervalId = setInterval(updateDuration, 1000 / 66);
-    };
-
-    //Функция обновления интервала
-    function updateDuration() {
-      durationControl.value = video.currentTime;
-    };
-
-    //Регултровка громкости
-    function soundOff() {
-
-      if (video.volume === 0) {
-        video.volume = soundLevel;
-        soundControl.value = soundLevel * 10;
-      } else {
-        soundLevel = video.volume;
-        video.volume = 0;
-        soundControl.value = 0;
-      }
-    };
-
-    //Выставление громкости
-    function changeSoundVolume() {
-      video.volume = soundControl.value / 10;
-    };
-  };
+  }
 })()
